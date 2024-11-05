48cef9505e67741ebd996d7f9515e1517824fedb
conta idriss <idriss1elouiri2@gmail.com>
2024-11-05 19:48:17 +0100

    add getCustomers and delete api route

--- backend/src/modules/customer/customer.controller.js
+++ backend/src/modules/customer/customer.controller.js
@@ -1,4 +1,4 @@
-import Customer from "./customer.model";
+import Customer from "./customer.model.js";
 
 export const createCustomer = async (req, res, next) => {
   const newCustomer = new Customer({
@@ -13,3 +13,43 @@ export const createCustomer = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getCustomers = async (req, res, next) => {
+    try {
+      const startIndex = parseInt(req.query.startIndex, 10) || 0;
+      const sortDirection = req.query.sort === 'asc' ? 1 : -1;
+  
+      const customers = await Customer.find()
+        .sort({ createdAt: sortDirection })
+        .skip(startIndex)
+  
+      const totalCustomers = await Customer.countDocuments();
+  
+      // Get the count of Customers created in the last month
+      const lastMonthCustomersCount = await Customer.countDocuments({
+        createdAt: { $gte: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000) },
+      });
+  
+      res.status(200).json({
+        customers,
+        totalCustomers,
+        lastMonthCustomers: lastMonthCustomersCount,
+      });
+    } catch (error) {
+      console.error("Error fetching customers:", error); // Log error for debugging
+      next(error);
+    }
+  };
+
+  export const deleteCustomer = async (req, res, next) => {
+    try {
+      const deletedCustomer = await Customer.findByIdAndDelete(req.params.customerId);
+      if (!deletedCustomer) {
+        return next(errorHandler(404, 'customer not found'));
+      }
+      res.status(200).json({ message: 'The customer has been deleted' });
+    } catch (error) {
+      console.error("Error deleting customer:", error); // Log error for debugging
+      next(error);
+    }
+  };
--- backend/src/modules/customer/customer.route.js
+++ backend/src/modules/customer/customer.route.js
@@ -4,5 +4,7 @@ import * as customerController from "./customer.controller.js";
 const router = express.Router();
 
 router.post("/createCustomer", customerController.createCustomer);
+router.post("/getCustomers", customerController.getCustomers);
+router.delete("/deletecustomer/:customerId", customerController.deleteCustomer);
 
 export default router;
