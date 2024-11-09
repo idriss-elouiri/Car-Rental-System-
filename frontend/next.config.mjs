8afae11a3aeb9d46e6d80ca4ac8cfb86d897fd73
conta idriss <idriss1elouiri2@gmail.com>
2024-11-09 14:09:38 +0100

    fix issue happen win i try to deploy in render

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: "build",
-    reactStrictMode: true,
-    env: {
-      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    },
-  };
-  
-  export default nextConfig;
-  
+  distDir: "build",
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/old-page",
+        destination: "/new-page",
+        permanent: true,
+      },
+    ];
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+};
+
+export default nextConfig;
